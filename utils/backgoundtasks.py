07900c009575ffacfda5589298abeda91d436d00
Derk Weijers <derk@derkweijers.nl>
2018-09-23 21:07:59 +0200

    WIP - transfer ot other laptop

--- utils/backgoundtasks.py
+++ utils/backgoundtasks.py
@@ -1,4 +1,5 @@
 import asyncio
+import redis
 from utils import spaceflightnewsapi
 
 
@@ -7,5 +8,7 @@ async def send_latest(client):
     await client.wait_until_ready()
     while not client.is_closed:
         latest_article = await spaceflightnewsapi.latest_article()
+        for article in latest_article:
+            if article['_id'] == redis.get
         await client.send_message(client.get_channel(id='491221005117685771'), latest_article)
         await asyncio.sleep(10)
