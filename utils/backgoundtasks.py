6018b9930086cc8e0a262fcd09f0bf09b582e85e
Derk Weijers <derk@derkweijers.nl>
2018-09-24 20:51:07 +0200

    Embed with the latest article is now pushed

--- utils/backgoundtasks.py
+++ utils/backgoundtasks.py
@@ -1,7 +1,11 @@
 import asyncio
+import os
+
+import discord
 import redis
 from utils import spaceflightnewsapi
 
+r = redis.Redis.from_url( os.environ['REDIS_URI'] )
 
 async def send_latest(client):
 
@@ -9,6 +13,10 @@ async def send_latest(client):
     while not client.is_closed:
         latest_article = await spaceflightnewsapi.latest_article()
         for article in latest_article:
-            if article['_id'] == redis.get
-        await client.send_message(client.get_channel(id='491221005117685771'), latest_article)
+            if article['_id'] != r.get('latest_id').decode():
+                embed = discord.Embed(title=article['title'], description=article['news_site_long'],
+                                      url=article['url'], color=2659031)
+                embed.set_image(url=article['featured_image'])
+                await client.send_message(client.get_channel(id='493854629747097610'), embed=embed) #TODO: Remove channel ID
+                r.set('latest_id', article['_id'])
         await asyncio.sleep(10)
