9ac88c64fb41e786721f53b2c3b68f4d3a9eef2b
Derk Weijers <derk@derkweijers.nl>
2018-11-25 10:46:50 +0100

    Send new tweet

--- utils/backgoundtasks.py
+++ utils/backgoundtasks.py
@@ -18,11 +18,12 @@ async def send_latest(client):
     while not client.is_closed:
         latest_article = await spaceflightnewsapi.latest_article()
         for article in latest_article:
-            if article['_id'] == r.get('latest_id').decode():
+            if article['_id'] != r.get('latest_id').decode():
                 for subscribed_channel in r.lrange('subscribed_channels', 0, -1):
                     embed = discord.Embed(title=article['title'], description=article['news_site_long'],
                                           url=article['url'], color=2659031)
                     embed.set_image(url=article['featured_image'])
                     await client.send_message(client.get_channel(id=subscribed_channel.decode()), embed=embed)
                 r.set('latest_id', article['_id'])
+                api.PostUpdate('New article by %s: %s %s' % (article['news_site_long'], article['title'], article['url']))
         await asyncio.sleep(10)
