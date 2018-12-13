46c3f111f59f14d1fb71de6f4fde059c05d76174
Derk Weijers <derkweijers@users.noreply.github.com>
2018-12-13 14:46:02 +0100

    Update backgoundtasks.py

--- utils/backgoundtasks.py
+++ utils/backgoundtasks.py
@@ -13,7 +13,7 @@ async def send_latest(client):
         latest_articles = await spaceflightnewsapi.latest_article()
         for article in latest_articles:
             if r.sismember('latest_articles', article['_id']) is False:
-                for subscribed_channel in r.lrange('subscribed_channel', 0, -1):
+                for subscribed_channel in r.lrange('subscribed_channels', 0, -1):
                     print(subscribed_channel)
                     print(article['_id'])
                     embed = discord.Embed(title=article['title'], description=article['news_site_long'],
