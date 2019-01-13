2f3e0fa886fd12ea1ad7e6e98523ea0a71beff10
Derk Weijers <derk@derkweijers.nl>
2019-01-13 18:28:08 +0100

    Preparation for Phys

--- utils/backgoundtasks.py
+++ utils/backgoundtasks.py
@@ -19,11 +19,11 @@ async def send_latest(client):
         latest_articles = await spaceflightnewsapi.latest_article()
         for article in latest_articles:
             if r.sismember('latest_articles', article['_id']) is False:
-                for subscribed_channel in r.lrange('subscribed_channels', 0, -1):
-                    embed = discord.Embed(title=article['title'], description=article['news_site_long'],
-                                          url=article['url'], color=2659031)
-                    embed.set_image(url=article['featured_image'])
-                    await client.send_message(client.get_channel(id=subscribed_channel.decode()), embed=embed)
+                # for subscribed_channel in r.lrange('subscribed_channel', 0, -1):
+                #     embed = discord.Embed(title=article['title'], description=article['news_site_long'],
+                #                           url=article['url'], color=2659031)
+                #     embed.set_image(url=article['featured_image'])
+                #     await client.send_message(client.get_channel(id=subscribed_channel.decode()), embed=embed)
                 r.sadd('latest_articles', article['_id'])
         await asyncio.sleep(60)
 
@@ -34,6 +34,6 @@ async def send_latest_twitter(client):
         latest_articles = await spaceflightnewsapi.latest_article()
         for article in latest_articles:
             if r.sismember('latest_articles_twitter', article['_id']) is False:
-                api.update_status('New article by %s: %s %s' % (article['news_site_long'], article['title'], article['url']))
+                # api.update_status('New article by %s: %s %s' % (article['news_site_long'], article['title'], article['url']))
                 r.sadd('latest_articles_twitter', article['_id'])
         await asyncio.sleep(60)
