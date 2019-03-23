e8424f05bcb660107dcc12d8054b5b5c9ad44cf5
Derk Weijers <derk@derkweijers.nl>
2019-03-23 14:21:35 +0100

    There should be no await here

--- utils/spaceflightnewsapi.py
+++ utils/spaceflightnewsapi.py
@@ -8,4 +8,4 @@ async def latest_article():
     async with aiohttp.ClientSession() as session:
         async with session.get(snapi_url) as response:
             articles = await response.json()
-            return await articles['docs']
+            return articles['docs']
