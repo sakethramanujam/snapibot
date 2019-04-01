d37525e37578d0e404bf2ec8e59638f8b750cd73
Derk Weijers <derk@derkweijers.nl>
2019-04-01 19:19:25 +0200

    Do not post ESA to discord

--- utils/backgoundtasks.py
+++ utils/backgoundtasks.py
@@ -18,14 +18,15 @@ async def send_latest(client):
     while not client.is_closed:
         latest_articles = await spaceflightnewsapi.latest_article()
         for article in latest_articles:
-            if r.sismember('latest_articles', article['_id']) is False:
-                for subscribed_channel in r.lrange('subscribed_channels', 0, -1):
-                    embed = discord.Embed(title=article['title'], description=article['news_site_long'],
-                                          url=article['url'], color=2659031)
-                    embed.set_image(url=article['featured_image'])
-                    await client.send_message(client.get_channel(id=subscribed_channel.decode()), embed=embed)
-                r.sadd('latest_articles', article['_id'])
-                print("Send new article to Discord " + article['title'])
+            if article['news_site'] is not "esa":
+                if r.sismember('latest_articles', article['_id']) is False:
+                    for subscribed_channel in r.lrange('subscribed_channels', 0, -1):
+                        embed = discord.Embed(title=article['title'], description=article['news_site_long'],
+                                              url=article['url'], color=2659031)
+                        embed.set_image(url=article['featured_image'])
+                        await client.send_message(client.get_channel(id=subscribed_channel.decode()), embed=embed)
+                    r.sadd('latest_articles', article['_id'])
+                    print("Send new article to Discord " + article['title'])
         await asyncio.sleep(60)
 
 
