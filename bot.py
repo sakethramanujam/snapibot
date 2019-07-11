f4bc99c1fb019ecdc7fe342c0f2d94d057e550aa
Derk Weijers <derk@derkweijers.nl>
2019-07-11 20:42:48 +0200

    Inital comit new version

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+import os
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run('NTk4NDkzOTgzODg2NTQwODAw.XSXdDA.gua_iudcsJ39tgD6i3WL6G6Rpio')
---
+++ cogs/articles.py
@@ -0,0 +1,31 @@
+from discord import Embed, Colour
+from discord.ext import commands
+import urllib.request
+import json
+
+
+class Articles(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Articles cog loaded')
+
+    @commands.command()
+    async def latest(self, ctx):
+        response = urllib.request.urlopen("https://www.spaceflightnewsapi.net/api/v1/articles?limit=5")
+        response = response.read().decode()
+        response = json.loads(response)
+
+        for article in response['docs']:
+            embed = Embed(title=article['title'], url=article['url'], color=Colour(value=0x2196f3))
+            embed.set_author(name=article['news_site_long'])
+            embed.set_image(url=article['featured_image'])
+            embed.set_footer(text="Powered by https://www.spaceflightnewsapi.net")
+            await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(Articles(client))
